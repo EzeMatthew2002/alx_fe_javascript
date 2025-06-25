5129c21cb478eec2bb96e325b529973a3c094931
Ezebong Matthew <ezebongmatthew490@gmail.com>
2025-06-25 17:25:31 +0100

    Dynamic Quote Generator

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -8,19 +8,29 @@ const quotes = [
   { text: "JavaScript is the duct tape of the Internet.", category: "Tech" },
 ];
 
-function addQuote() {
-  let createAddQuoteForm = document.getElementById("newQuoteText");
-  let categoryInput = document.getElementById("newQuoteCategory");
+const formArea = document.getElementById("formArea");
 
-  let text = createAddQuoteForm.value;
-  let category = categoryInput.value;
+const quoteInput = document.createElement("input");
+quoteInput.type = "text";
+quoteInput.id = "newQuoteText";
+quoteInput.placeholder = "Enter a new quote";
+
+const categoryInput = document.createElement("input");
+categoryInput.type = "text";
+categoryInput.id = "newQuoteCategory";
+categoryInput.placeholder = "Enter quote category";
+
+const addBtn = document.createElement("button");
+addBtn.textContent = "Add Quote";
+addBtn.onclick = function () {
+  const text = quoteInput.value.trim();
+  const category = categoryInput.value.trim();
 
   if (text && category) {
     const newQuote = {
-      text: text,
-      category: category,
+      text,
+      category,
     };
-
     quotes.push(newQuote);
 
     quoteInput.value = "";
@@ -28,9 +38,14 @@ function addQuote() {
   } else {
     alert("Please enter both quote text and category");
   }
-}
+};
 
-function  displayRandomQuote() {
+
+formArea.appendChild(quoteInput);
+formArea.appendChild(categoryInput);
+formArea.appendChild(addBtn);
+
+function displayRandomQuote() {
   const showRandomQuote = Math.floor(Math.random() * quotes.length);
   const quoteOutput = quotes[showRandomQuote];
   quoteDisplay.innerHTML = `"${quoteOutput.text}" - ${quoteOutput.category}`;
