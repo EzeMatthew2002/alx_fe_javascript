0de7ece88a89f6ca532c57795502cc70afffb131
Ezebong Matthew <ezebongmatthew490@gmail.com>
2025-06-25 12:06:48 +0100

    Dynamic Quote Generator

---
+++ dom-manipulation/script.js
@@ -0,0 +1,39 @@
+let quoteDisplay = document.getElementById("quoteDisplay");
+const showNewQuote = document.getElementById("newQuote");
+const quotes = [
+  {
+    text: "The best way the to learn is by ",
+    category: "studying",
+  },
+  { text: "JavaScript is the duct tape of the Internet.", category: "Tech" },
+];
+
+function addQuote() {
+  let quoteInput = document.getElementById("newQuoteText");
+  let categoryInput = document.getElementById("newQuoteCategory");
+
+  let text = quoteInput.value;
+  let category = categoryInput.value;
+
+  if (text && category) {
+    const newQuote = {
+      text: text,
+      category: category,
+    };
+
+    quotes.push(newQuote);
+
+    quoteInput.value = "";
+    categoryInput.value = "";
+  } else {
+    alert("Please enter both quote text and category");
+  }
+}
+
+function showRandomQuote() {
+  const random = Math.floor(Math.random() * quotes.length);
+  const quoteOutput = quotes[random];
+  quoteDisplay.textContent = `"${quoteOutput.text}" - ${quoteOutput.category}`;
+}
+
+showNewQuote.addEventListener("click", showRandomQuote);
